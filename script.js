1048f3d9c531f4eea2ec5217e5a2a5c430290809
syed-sameer1 <48875785+syed-sameer1@users.noreply.github.com>
2019-06-10 21:14:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// pseudo code (code in yoyr own words)
+
+/*problem statement: show user input inside heading
+
+   1. get inpute lement on which user typed something.
+   2. get value on inpute.
+   3. get heading element.
+   4. change heading content.
+
+*/
+
+/*
+   -how get an html elemnet.
+   -how to get an attribute of an html element.
+   -how ot get content of an html elemnet.
+*/
+
+
+function changeName() {
+   //get input element.
+   //get value of inpute element.
+   //get heading.
+
+   document.querySelector("h1 > #first").innerHTML = document.querySelector(".first").value;
+   document.querySelector("h1 > #last").innerHTML = document.querySelector(".last").value;
+
+}
+
+// function changeColor(){
+//    document.querySelector("body").style = "background-color: "+ document.querySelector("input").value;
+// }
